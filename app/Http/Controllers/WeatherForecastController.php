f797b06eead6a90c46abbff586bbb2fc9fa1f8eb
Viktoria Rakhely <m.rakhely.viktoria@gmail.com>
2023-02-02 11:36:02 +0100

    created WeatherForecastService refactored getWeather() function in WeatherForecastController created WeatherForecastCommand

--- app/Http/Controllers/WeatherForecastController.php
+++ app/Http/Controllers/WeatherForecastController.php
@@ -3,16 +3,25 @@
 namespace App\Http\Controllers;
 
 use App\Models\API\APIcalling;
+use App\Service\WeatherForecastService;
 use Illuminate\Http\Request;
 
 class WeatherForecastController extends Controller
 {
-    //
+
+    private WeatherForecastService $weatherForecastService;
+
+    public function __construct(WeatherForecastService $weatherForecastService)
+    {
+
+        $this->weatherForecastService = $weatherForecastService;
+    }
     public function getWeather()
     {
 
         $get_data = APIcalling::callAPI('GET', 'https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&hourly=temperature_2m', false);
         $response = json_decode($get_data, true);
+        $this->weatherForecastService->processApiResponse($response);
 
     }
 }
