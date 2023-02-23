bc3072aa27e9be11328d57ba069de670160789ac
Viktoria Rakhely <m.rakhely.viktoria@gmail.com>
2023-02-23 11:10:03 +0100

    Deleted HistoricalWeatherHumidityModel.php properties

--- app/Models/HistoricalWeatherHumidityModel.php
+++ app/Models/HistoricalWeatherHumidityModel.php
@@ -9,10 +9,6 @@ class HistoricalWeatherHumidityModel extends Model
 {
     use HasFactory;
 
-    public float $longitude;
-    public float $latitude;
-    public int $relative_humidity_2m;
-    public string $date_time_of_measurement;
     protected $fillable = [
         'latitude',
         'longitude',
