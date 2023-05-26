d9afdedbeb2509d3984c4e376c06d18bd69e19a3
rea <m.rakhely.viktoria@gmail.com>
2023-05-26 11:58:39 +0200

    Deleted HistoricalWeatherTenYearDatesCreator.php

--- app/Providers/ElasticLogProvider.php
+++ app/Providers/ElasticLogProvider.php
@@ -15,7 +15,7 @@ class ElasticLogProvider extends ServiceProvider
      *
      * @return void
      */
-    public function register()
+    public function register(): void
     {
         //
     }
@@ -25,7 +25,7 @@ class ElasticLogProvider extends ServiceProvider
      *
      * @return void
      */
-    public function boot()
+    public function boot(): void
     {
         $index = rtrim(config('elastic_log.suffix'), '_') . '_' . config('elastic_log.index');
         $type = config('elastic_log.type');
