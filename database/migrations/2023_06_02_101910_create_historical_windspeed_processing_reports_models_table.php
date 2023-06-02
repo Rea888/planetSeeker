485a5aa298ebce2f07735416c91bf1ad1b6b2802
rea <m.rakhely.viktoria@gmail.com>
2023-06-02 12:22:56 +0200

    Created 2023_06_02_101638_create_historical_windspeed_models_table.php, 2023_06_02_101910_create_historical_windspeed_processing_reports_models_table.php migrations and HistoricalWindspeedModel.php, HistoricalWindspeedModelProcessingReportsModel.php models

---
+++ database/migrations/2023_06_02_101910_create_historical_windspeed_processing_reports_models_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('historical_windspeed_processing_reports_models', function (Blueprint $table) {
+            $table->id();
+            $table->string('year');
+            $table->string('month');
+            $table->string('city');
+            $table->dateTime('processing_began_at')->nullable();
+            $table->dateTime('processing_finished_at')->nullable();
+            $table->timestamps();
+
+            $table->unique(['year', 'month', 'city'], 'city_date_processing_unique_windspeed_process');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('historical_windspeed_processing_reports_models');
+    }
+};
